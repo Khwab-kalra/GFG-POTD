602d1077241d3fb51fd398db67123ac58bf071ce
KHWAB KALRA <58007063+Khwab-kalra@users.noreply.github.com>
2024-10-29 00:09:30 +0530

    Update 28.py

--- 2024/October/28.py
+++ 2024/October/28.py
@@ -9,3 +9,17 @@ class Solution:
             if i not in a:
                 a.append(i)
         return a
+
+# Optimised Time Performance
+
+class Solution:
+    def removeDuplicates(self, arr):
+        check = [0] * 101
+        ans = []
+        for num in arr:
+            if check[num] == 0:
+                ans.append(num)  # Add to the result if not seen
+                check[num] = 1   # Mark as seen
+        
+        return ans
+            
